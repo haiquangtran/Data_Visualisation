e4dea2e6d969b04491e14763dd39b74041d9c59b
haiquangtran <hai.quang.tran@gmail.com>
2015-08-29 05:47:27 +1200

    initialise app.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pisaVisualisationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pisaVisualisationApp
+ */
+angular.module('pisaVisualisationApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pisaVisualisationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pisaVisualisationApp
+ */
+angular.module('pisaVisualisationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
