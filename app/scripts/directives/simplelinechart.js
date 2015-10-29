701101adc96a9e7ffaadf55cef405d6ee7894d64
haiquangtran <hai.quang.tran@gmail.com>
2015-10-29 21:36:09 +1300

    bar graph of qualifications for father

--- app/scripts/directives/simplelinechart.js
+++ app/scripts/directives/simplelinechart.js
@@ -21,11 +21,33 @@ angular.module('pisaVisualisationApp')
             }
 
             d3.csv(fileName, function (data) {
-              console.log(data);
+              var fatherQualifications = [0,0,0,0];
 
               data.forEach(function (d) {
+                if (d["PA03Q01"] === "Yes") {
+                  fatherQualifications[0]++;
+                }
+                if (d["PA03Q02"] === "Yes") {
+                  fatherQualifications[1]++;
+                }
+                if (d["PA03Q03"] === "Yes") {
+                  fatherQualifications[2]++;
+                }
+                if (d["PA03Q04"] === "Yes") {
+                  fatherQualifications[3]++;
+                }
                 return d;
               });
+
+              console.log(fatherQualifications);
+              var chart = d3.select(element[0]);
+              chart.append("div").attr("class", "chart")
+                .selectAll('div')
+                .data(fatherQualifications).enter().append("div")
+                .transition().ease("elastic")
+                .style("width", function(d) { return d/1000 + "%"; })
+                .text(function(d) { return d + "%"; });
+
             });
           });
 
