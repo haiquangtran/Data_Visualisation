b7f03602f7e188b90d503590bcd1a54179f69a46
haiquangtran <hai.quang.tran@gmail.com>
2015-11-02 11:09:21 +1300

    working on heatmap Refactoring heatmap for the use of pisa data. Started doing that.

--- app/scripts/directives/heatmaptwo.js
+++ app/scripts/directives/heatmaptwo.js
@@ -28,9 +28,8 @@ angular.module('pisaVisualisationApp')
               buckets = 9,
               colors = ['#A0CAA0', '#66C266', '#007A00', '#005C00', '#003D00', '#001F00'],
               expectations = ["ISCED L3A", "ISCED L4", "ISCED L5B", "ISCED L5A,6"],
-            times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
-              //times = [ "Less than <$A>", "<$A> or more but less than <$B>", "<$B> or more but less than <$C>",
-              //  "<$C> or more but less than <$D>", "<$D> or more but less than <$E>", "<$E> or more"];
+            //times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+              times = [ "Less than <$A>", "<$A> or more but less than <$B>", "<$B> or more but less than <$C>", "<$C> or more but less than <$D>", "<$D> or more but less than <$E>", "<$E> or more"];
             var svg = d3.select("#chart").append("svg")
               .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.top + margin.bottom)
@@ -45,7 +44,7 @@ angular.module('pisaVisualisationApp')
               .attr("y", function (d, i) { return i * gridSize; })
               .style("text-anchor", "end")
               .attr("transform", "translate(-6," + gridSize / 1.5 + ")");
-              //.attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+            //.attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
             var timeLabels = svg.selectAll(".timeLabel")
               .data(times)
@@ -57,29 +56,27 @@ angular.module('pisaVisualisationApp')
               .attr("transform", "translate(" + gridSize / 2 + ", -6)")
               .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); })
 
-
             var heatmapChart = function(tsvFile) {
-              d3.tsv(tsvFile,
-                function(d) {
+              d3.tsv(tsvFile, function(d) {
                   return {
-                    day: +d.day,
-                    hour: +d.hour,
-                    value: +d.value
+                    expectation: + d.expectation,
+                    salary: + d.salary,
+                    frequency: + d.frequency
                   };
                 },
                 function(error, data) {
                   var colorScale = d3.scale.quantile()
-                    .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
+                    .domain([0, buckets - 1, d3.max(data, function (d) { return d.frequency; })])
                     .range(colors);
 
                   var cards = svg.selectAll(".hour")
-                    .data(data, function(d) {return d.day+':'+d.hour;});
+                    .data(data, function(d) {return d.expectation+':'+d.salary;});
 
                   cards.append("title");
 
                   cards.enter().append("rect")
-                    .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-                    .attr("y", function(d) { return (d.day - 1) * gridSize; })
+                    .attr("x", function(d) { return (d.salary - 1) * gridSize; })
+                    .attr("y", function(d) { return (d.expectation - 1) * gridSize; })
                     .attr("rx", 4)
                     .attr("ry", 4)
                     .attr("class", "hour bordered")
@@ -88,9 +85,9 @@ angular.module('pisaVisualisationApp')
                     .style("fill", colors[0]);
 
                   cards.transition().duration(1000)
-                    .style("fill", function(d) { return colorScale(d.value); });
+                    .style("fill", function(d) { return colorScale(d.frequency); });
 
-                  cards.select("title").text(function(d) { return d.value; });
+                  cards.select("title").text(function(d) { return d.frequency; });
 
                   cards.exit().remove();
 
