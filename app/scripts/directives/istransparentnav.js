e45d8ea9c5a1ca648e2f911b58c487d8d0b97ef2
haiquangtran <hai.quang.tran@gmail.com>
2015-11-02 13:44:36 +1300

    added hero banner

---
+++ app/scripts/directives/istransparentnav.js
@@ -0,0 +1,38 @@
+'use strict';
+/* TODO: put this into a directive */
+
+var transparent = false;
+
+var navBar = {
+  checkScrollForTransparentNavbar: debounce(function () {
+    if ($(document).scrollTop() > 260) {
+      if (transparent) {
+        transparent = false;
+        $('nav[role="navigation"]').removeClass('navbar-transparent');
+      }
+    } else {
+      if (!transparent) {
+        transparent = true;
+        $('nav[role="navigation"]').addClass('navbar-transparent');
+      }
+    }
+  }, 17)
+};
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		}, wait);
+		if (immediate && !timeout) func.apply(context, args);
+	};
+};
