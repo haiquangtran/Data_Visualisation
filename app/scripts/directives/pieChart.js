e17b5e9877cab8626070c561222b997b108cdf5e
haiquangtran <hai.quang.tran@gmail.com>
2015-11-04 05:17:08 +1300

    added mock pie graph

---
+++ app/scripts/directives/pieChart.js
@@ -0,0 +1,83 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name pisaVisualisationApp.directive:pieChart
+ * @description
+ * # pieChart
+ */
+angular.module('pisaVisualisationApp')
+  .directive('pieChart', function (d3Service, preprocessorHelper) {
+    return {
+      restrict: 'E',
+      replace: false,
+      scope: {data: '=chartData'},
+      link: function postLink(scope, element, attrs) {
+        d3Service.d3().then(function(d3) {
+
+          scope.$watch('data', function(fileName) {
+            if (!fileName) {
+              return;
+            }
+
+            var margin = { top: 50, right: 10, bottom: 100, left: 100 };
+            var w = 400;
+            var h = 400;
+            var r = h/3;
+            var colours = ['#A0CAA0', '#66C266', '#007A00', '#005C00', '#003D00', '#001F00'];
+
+            var pieData = [{"label":"Category A", "value":20},
+              {"label":"Category B", "value":50},
+              {"label":"Category C", "value":30}];
+
+            var pieChart = function(fileName) {
+              d3.csv(fileName, function(d) {
+                return {
+                  expectation: d.expectation,
+                  salary: d.salary,
+                  frequency: parseInt(d.frequency)
+                };
+              }, function(error, data) {
+
+
+                var vis = d3.select(".chartBackdrop")
+                  .append("svg:svg")
+                  .data([pieData])
+                  .attr("width", w)
+                  .attr("height", h)
+                  .append("svg:g")
+                  .attr("transform", "translate(" + (r + margin.left)  + "," + (r + margin.top) + ")");
+
+                var pie = d3.layout.pie().value( function(d){ return d.value; });
+                // declare an arc generator function
+                var arc = d3.svg.arc().outerRadius(r);
+                // select paths, use arc generator to draw
+                var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+                arcs.append("svg:path")
+                  .attr("fill", function(d, i){
+                    return colours[i];
+                  })
+                  .attr("d", function (d) {
+                    // log the result of the arc generator to show how cool it is :)
+                    console.log(arc(d));
+                    return arc(d);
+                  });
+                // add the text
+                arcs.append("svg:text").attr("transform", function(d){
+                  d.innerRadius = 0;
+                  d.outerRadius = r;
+                  return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+                    return pieData[i].label;}
+                );
+
+
+              });
+            };
+
+            pieChart(fileName);
+
+          });
+        });
+      }
+    };
+  });
