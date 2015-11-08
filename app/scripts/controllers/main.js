70c139a3ec4a7d70139181ccb8237d85606538a3
haiquangtran <hai.quang.tran@gmail.com>
2015-11-09 07:00:22 +1300

    added angular-scroll js. Starting to test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,31 @@
  * # MainCtrl
  * Controller of the pisaVisualisationApp
  */
-angular.module('pisaVisualisationApp')
-  .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
-
-
-  });
+//angular.module('pisaVisualisationApp',  ['duScroll'])
+//  .controller('MainCtrl', function ($scope, $location, $anchorScroll, $document) {
+//    //var container = angular.element(document.getElementById('container'));
+//    //var section2 = angular.element(document.getElementById('parents'));
+//    //
+//    //$scope.toTheTop = function() {
+//    //  container.scrollTop(0, 5000);
+//    //}
+//    //
+//    //$scope.toSection2 = function() {
+//    //  container.scrollTo(section2, 0, 1000);
+//    //}
+//
+//    )
+//  });
+angular.module('pisaVisualisationApp', ['duScroll']).
+  controller('MainCtrl', function($scope, $document){
+    $scope.toTheTop = function() {
+      $document.scrollTopAnimated(0, 5000).then(function() {
+        console && console.log('You just scrolled to the top!');
+      });
+    }
+    var section3 = angular.element(document.getElementById('section-3'));
+    $scope.toSection3 = function() {
+      $document.scrollToElementAnimated(section3);
+    }
+  }
+).value('duScrollOffset', 30);
