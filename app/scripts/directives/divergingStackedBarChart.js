761bb0ef4c73274a05c51bd42e89424a0844ed85
haiquangtran <hai.quang.tran@gmail.com>
2015-11-10 02:41:17 +1300

    trying to udpate

--- app/scripts/directives/divergingStackedBarChart.js
+++ app/scripts/directives/divergingStackedBarChart.js
@@ -183,6 +183,141 @@ angular.module('pisaVisualisationApp')
             });
           };
 
+
+          function updateStackedGraph(fileName) {
+            d3.csv(fileName, function(error, data) {
+              data.forEach(function (d) {
+                // calc percentages
+                d["Strongly disagree"] = +d[1] * 100 / d.N;
+                d["Disagree"] = +d[2]  * 100 / d.N;
+                d["Agree"] = +d[3]  * 100 / d.N;
+                d["Strongly agree"] = +d[4]  * 100 / d.N;
+                var x0 = -1 * (d["Agree"] / 2 + d["Disagree"] + d["Strongly disagree"]);
+                var idx = 0;
+                d.boxes = color.domain().map(function (name) {
+                  return {
+                    name: name, x0: x0, x1: x0 += +d[name], N: +d.N, n: +d[idx+=1]
+                  };
+                });
+              });
+
+              var min_val = d3.min(data, function (d) {
+                return d.boxes["0"].x0;
+              });
+              var max_val = d3.max(data, function (d) {
+                return d.boxes["3"].x1;
+              });
+
+              x.domain([min_val, max_val]).nice();
+              y.domain(data.map(function (d) {
+                return d.Question;
+              }));
+
+              svg.select('.chartBack').select("g.x axis")
+                .call(xAxis);
+
+              svg.append("g")
+                .selectAll("g.y axis")
+                .call(yAxis);
+              
+              // TODO: THIS IS THE MAIN ONE TO UDPATE DO THIS!
+              var vakken = svg.selectAll(".question")
+                .data(data)
+                .enter().append("g")
+                .attr("class", "bar")
+                .attr("transform", function (d) {
+                  return "translate(0," + y(d.Question) + ")";
+                });
+
+              var bars = vakken.selectAll("rect")
+                .data(function (d) {
+                  return d.boxes;
+                })
+                .enter().append("g").attr("class", "subbar");
+
+              bars.append("rect")
+                .attr("height", y.rangeBand())
+                .attr("x", function (d) {
+                  return x(d.x0);
+                })
+                .attr("width", function (d) {
+                  return x(d.x1) - x(d.x0);
+                })
+                .style("fill", function (d) {
+                  return color(d.name);
+                });
+
+              bars.append("text")
+                .attr("x", function (d) {
+                  return x(d.x0);
+                })
+                .attr("y", y.rangeBand() / 2)
+                .attr("dy", "0.5em")
+                .attr("dx", "0.5em")
+                .style("font", "10px sans-serif")
+                .style("text-anchor", "begin")
+                .text(function (d) {
+                  return d.n !== 0 && (d.x1 - d.x0) > 3 ? d.n : ""
+                });
+
+              vakken.insert("rect", ":first-child")
+                .attr("height", y.rangeBand())
+                .attr("x", "1")
+                .attr("width", width)
+                .attr("fill-opacity", "0.5")
+                .style("fill", "#F5F5F5")
+                .attr("class", function (d, index) {
+                  return index % 2 == 0 ? "even" : "uneven";
+                });
+
+              svg.append("g")
+                .attr("class", "y axis")
+                .append("line")
+                .attr("x1", x(0))
+                .attr("x2", x(0))
+                .attr("y2", height);
+
+              var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
+              // this is not nice, we should calculate the bounding box and use that
+              var legend_tabs = [0, 120, 200, 375, 450];
+              var legend = startp.selectAll(".legend")
+                .data(color.domain().slice())
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function (d, i) {
+                  return "translate(" + legend_tabs[i] + ",-45)";
+                });
+
+              legend.append("rect")
+                .attr("x", 0)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", color);
+
+              legend.append("text")
+                .attr("x", 22)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "begin")
+                .style("font", "10px sans-serif")
+                .text(function (d) {
+                  return d;
+                });
+
+              d3.selectAll(".axis path")
+                .style("fill", "none")
+                .style("stroke", "#000")
+                .style("shape-rendering", "crispEdges")
+
+              d3.selectAll(".axis line")
+                .style("fill", "none")
+                .style("stroke", "#000")
+                .style("shape-rendering", "crispEdges")
+
+              var movesize = width / 2 - startp.node().getBBox().width / 2;
+              d3.selectAll(".legendbox").attr("transform", "translate(" + movesize + ",0)");
+            });
+          };
           scope.$watchGroup(['data', 'selectedExpectation'], function(newValues, oldValues, scope) {
             var fileName = newValues[0];
             var selectedExpectation = newValues[1];
@@ -190,10 +325,8 @@ angular.module('pisaVisualisationApp')
               return;
             }
 
-            if (selectedExpectation != oldValues[1]) {
-              //updateBarChart(fileName, selectedExpectation, selectedSalary);
-              createStackedGraph(fileName, selectedExpectation);
-
+            if (fileName != oldValues[0]) {
+              updateStackedGraph(fileName, selectedExpectation);
             } else {
               createStackedGraph(fileName, selectedExpectation);
             }
